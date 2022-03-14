c6d297211f5ff79596942da36f62ab912538ef6b
FiniteWill <60437388+FiniteWill@users.noreply.github.com>
2022-03-13 23:35:38 -0500

    Simple Item Pickup
    Created a simple item pickup for the player.

---
+++ Point_Plaza_Game/Assets/Scripts/Item/ItemDefinitions.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDefinitions : MonoBehaviour
+{}
+
+public enum Item
+{
+    Debug,
+    SmallHealthBoost
+};
---
+++ Point_Plaza_Game/Assets/Scripts/Item/ItemPickup.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class ItemPickup : MonoBehaviour
+{
+    [SerializeField] private GameObject item = null;
+    [SerializeField] private string[] interactibleTags = null;
+    [SerializeField] private Item itemDefinition = Item.Debug; 
+
+    private void Awake()
+    {
+        Assert.IsNotNull($"{this.name} does not have a {nameof(item)} but requires one.");
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision != null)
+        {
+            foreach (string tag in interactibleTags)
+            {
+                if (collision.CompareTag(tag)) { HandleEffect(); }
+            }
+        }
+    }
+
+    private void HandleEffect()
+    {
+        gameObject.SetActive(false);
+
+        // TODO: Degine behaviors for item pickup
+        switch(itemDefinition)
+        {
+            case Item.Debug:
+                Debug.Log($"{this.name} was successfully collected.");
+                break;
+            case Item.SmallHealthBoost:
+                break;
+            default:
+                break;
+        }
+    }
+}
