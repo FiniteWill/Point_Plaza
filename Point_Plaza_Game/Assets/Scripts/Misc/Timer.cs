70d3a0e6b95bca256323e140e7a30e7f9813bf7d
FiniteWill <60437388+FiniteWill@users.noreply.github.com>
2022-03-15 17:38:38 -0500

    Working Timer & File Management

--- Point_Plaza_Game/Assets/Scripts/Misc/Timer.cs
+++ Point_Plaza_Game/Assets/Scripts/Misc/Timer.cs
@@ -7,18 +7,29 @@ using UnityEngine.UI;
 public class Timer : MonoBehaviour
 {
     [SerializeField] private Text display = null;
-    private float timer;
+    private int seconds = 0;
+    private int minutes = 0;
+    private WaitForSecondsRealtime oneSecond = new WaitForSecondsRealtime(1f);
 
     private void Awake()
     {
         Assert.IsNotNull(display, $"{this.name} does not have a {nameof(display)} but requires one.");
+        StartCoroutine(Tick());
     }
 
-    private void Update()
+
+    IEnumerator Tick()
     {
-        timer += Time.deltaTime;
-        int minutes = Mathf.FloorToInt(timer / 60F);
-        int seconds = Mathf.FloorToInt(timer % 60F);
-        display.text = "TIME " + minutes.ToString("00") + ":" + seconds.ToString("00");
+        if(seconds == 60)
+        {
+            seconds = 0;
+            minutes++;
+        }
+        else
+        { seconds++; }
+        display.text = "TIME " +minutes.ToString("00") + ":" +seconds.ToString("00");
+        yield return oneSecond;
+        if(minutes < 60)
+        { StartCoroutine(Tick()); }
     }
 }
