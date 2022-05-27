5a49fd05f341edf32556cf1e68c3bcb1b1ed4d70
FiniteWill <60437388+FiniteWill@users.noreply.github.com>
2022-05-27 02:02:01 -0500

    Generic Damaging Zone
    Converted the Kill Zone script into a generic damaging zone that can do damage or completely deplete an entities health.

---
+++ Point_Plaza_Game/Assets/Scripts/Level/Interactible/DamageZone.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+[RequireComponent(typeof(Collider2D))]
+public class DamageZone : MonoBehaviour
+{
+    [SerializeField] private bool isDebugging = false;
+    [SerializeField] private bool isKillZone = false;
+    [SerializeField] private int damageToDeal = 1;
+    [SerializeField] private float damageDelay = 3f;
+    [SerializeField] private List<string> interactibleTags = null;
+
+    private Collider2D collider;
+    private List<Health> damagedEntities = new List<Health>();
+
+    public bool isPaused = false;
+
+    private void Awake()
+    {
+        Assert.IsNotNull(interactibleTags, $"{name} does not have any tags specified for interaction.");
+        collider = GetComponent<Collider2D>();
+        collider.isTrigger = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!isPaused)
+        {
+            if (isDebugging) { Debug.Log($"{other.name} has triggered {name}"); }
+
+            foreach (string tag in interactibleTags)
+            {
+                if (other.CompareTag(tag))
+                {
+                    // Affect health
+                    Health entityHealth = other.GetComponent<Health>();
+                    if (entityHealth != null)
+                    {
+                        if (isKillZone) { entityHealth.SetHealth(0); }
+                        else 
+                        { 
+                            entityHealth.TakeDamage(damageToDeal);
+                            StartCoroutine(Delay(damageDelay, entityHealth));
+                        }
+                    }
+                    else if(isDebugging){ Debug.Log($"{other.name} does not have an attached {nameof(entityHealth)}"); }
+                }
+            }
+        }
+    }
+
+    private IEnumerator Delay(float delay, Health entityHealth)
+    {
+        if (!damagedEntities.Contains(entityHealth))
+        {
+            damagedEntities.Add(entityHealth);
+            yield return new WaitForSeconds(delay);
+            damagedEntities.Remove(entityHealth);
+        }
+        yield return null;
+    }
+}
