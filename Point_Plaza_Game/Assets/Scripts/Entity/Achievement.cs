bbbfa291c194872e91841c3da123fe7916392851
FiniteWill <60437388+FiniteWill@users.noreply.github.com>
2022-08-14 05:20:30 -0500

    Achievement
    Created a class for holding data related to tracking an achievement. Also removing these annoying log files that should be untracked and pushing build settings.

---
+++ Point_Plaza_Game/Assets/Scripts/Entity/Achievement.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Holds data for an ahievement including its completion metrics and how to earn it.
+/// </summary>
+public class Achievement
+{
+    private string achievementName = "Default Achievement";
+    private string achievementDescription = "This is a default achievement.";
+    private bool isCompleted = false;
+    private float completionPercentage = 0f;
+    private bool displayPercentage = false;
+    private Dictionary<string, bool> completionMetrics = new Dictionary<string, bool>() { { "Default metric", false } };
+
+    
+    public void MarkMetric(string metricName, bool status)
+    {
+        if(completionMetrics.ContainsKey(metricName))
+        {
+            completionMetrics.Remove(metricName);
+            completionMetrics.Add(metricName, status);
+        }
+        CheckCompletion();
+    }
+
+    private void CheckCompletion()
+    {
+        int numCompleted = 0;
+        int numTotal = completionMetrics.Keys.Count;
+        foreach(KeyValuePair<string,bool> metric in completionMetrics)
+        {
+            if (metric.Value == false)
+            {
+                isCompleted = false;
+            }
+            else
+            {
+                numCompleted++;
+            }
+        }
+
+        if (numCompleted == numTotal)
+        {
+            isCompleted = true;
+        }
+
+        completionPercentage = numCompleted / (float)numTotal;
+    }
+
+
+}
