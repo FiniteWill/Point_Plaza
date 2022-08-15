a769c3397ed8b3bfa61279504a94f482637cf60d
FiniteWill <60437388+FiniteWill@users.noreply.github.com>
2022-08-15 01:49:52 -0500

    Scriptable Object setup for Items and Achievements

---
+++ Point_Plaza_Game/Assets/ScriptableObjects/Achievement SOs/AchievementSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Achievement", menuName = "ScriptableObjects/AchievementSO", order = 1)]
+public class AchievementSO : ScriptableObject
+{
+    public string meme;
+    public int test;
+}
