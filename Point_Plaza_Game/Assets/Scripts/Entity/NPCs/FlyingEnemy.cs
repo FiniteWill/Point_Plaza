39d71c6ff5822640a33118999b6837f1932cdceb
FiniteWill <60437388+FiniteWill@users.noreply.github.com>
2022-12-14 05:46:53 -0600

    SFX and Art Import along with setup for flying enemy
    Import of SFX assets and some quick cloud art sprites for background flavor.

---
+++ Point_Plaza_Game/Assets/Scripts/Entity/NPCs/FlyingEnemy.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+/// <summary>
+/// 
+/// </summary>
+[RequireComponent(typeof(DetectionZone)]
+public class FlyingEnemy : MonoBehaviour
+{
+
+    private bool playerInRange = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if()
+    }
+}
