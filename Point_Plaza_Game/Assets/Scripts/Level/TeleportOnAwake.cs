d4bf098cab09efd5470701e401d04ae129408c4b
FiniteWill <60437388+FiniteWill@users.noreply.github.com>
2022-12-14 17:58:38 -0600

    Fixes

---
+++ Point_Plaza_Game/Assets/Scripts/Level/TeleportOnAwake.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportOnAwake : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform spawn;
+    private float delay = 2f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Teleport());
+    }
+
+    IEnumerator Teleport()
+    {
+        yield return new WaitForSeconds(delay);
+        player.position = spawn.position;
+    }
+}
